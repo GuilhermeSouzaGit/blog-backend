33b6a5fe88e7b4445114fe8b02b32a62821f0234
Guilherme <guisou1902@gmail.com>
2023-02-27 23:46:26 -0300

    added the middleware to post-routes

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,6 @@
+//token user = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiR3VpbGhlcm1lIiwiaWQiOiI2M2VkMzQwOTI3NGUwMGIyMWJhYjRkOGQiLCJpc0FkbWluIjpmYWxzZSwiaWF0IjoxNjc3NTQwNzM1fQ.SKGD0m_8MF8R2Z1UzP7oASzhGIlB0Iasmp1K8wLFfQE
+// token adm = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiYWRtaW4iLCJpZCI6IjYzZWQzMjM5NzQ4MjBjZTYyMTk0ZGJlMCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3NzU0MDU5MX0.vlTt0cKaOGkzJtbAZQcD1HhI0v2SLxh35q12RX2-jkg
+
 const express = require("express")
 const cors = require("cors")
 
@@ -18,8 +21,10 @@ app.use(express.static("public"))
 //Routes
 const UserRoutes = require("./routes/UserRoutes")
 const AdminRoutes = require("./routes/AdminRoutes")
+const PostRoutes = require("./routes/PostRoutes")
 
 app.use("/users", UserRoutes)
 app.use("/admin", AdminRoutes)
+app.use("/posts", PostRoutes)
 
 app.listen(5000)
