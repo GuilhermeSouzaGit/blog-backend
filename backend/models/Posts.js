b6dcd4bbd8a00588bbd0d96a5e1a99784d8a16d7
Guilherme <guisou1902@gmail.com>
2023-03-02 00:53:50 -0300

    added the field comments, likesCount and likes

--- backend/models/Posts.js
+++ backend/models/Posts.js
@@ -2,11 +2,35 @@ const mongoose = require("../db/conn")
 const {Schema} = mongoose
 
 const PostSchema = new Schema({
-    title: { type: String, required: true },
-    content: { type: String, required: true },
-    author: { type: String, required: true },
-    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment"}],
-    likesCount: { type: Number, default: 0 },
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    comments: [{
+        text: {
+            type: String,
+            required: true
+        },
+        postedBy: {
+            type: String
+        }
+    }],
+    likesCount: {
+        type: Number, 
+        default: 0 
+    },
+    likes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }]
 },{timestamps: true})
 
 const Posts = mongoose.model("Posts", PostSchema)
