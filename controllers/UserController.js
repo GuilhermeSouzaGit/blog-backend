23a2979e757cf49697b88eccf7f919ecc3c8a6cc
Guilherme <guisou1902@gmail.com>
2023-07-07 17:35:51 -0300

    feat: now users can see liked posts from other users

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User")
+const Posts = require("../models/Posts")
 const bcrypt = require("bcrypt")
 const nodemailer = require("nodemailer")
 
@@ -9,18 +10,18 @@ const getUserByToken = require("../helpers/get-user-by-token")
 
 module.exports = class UserController {
     static async register(req, res) {
-        const {name, email, password, confirmpassword} = req.body
+        const { name, email, password, confirmpassword } = req.body
 
         //validations
-        if(!name) return res.status(422).json({message: "O nome é obrigatório!"})
-        if(!email) return res.status(422).json({message: "O e-mail é obrigatório!"})
-        if(!password) return res.status(422).json({message: "A senha é obrigatória!"})
-        if(password !== confirmpassword) return res.status(422).json({message: "As senhas não conferem!"})
+        if (!name) return res.status(422).json({ message: "O nome é obrigatório!" })
+        if (!email) return res.status(422).json({ message: "O e-mail é obrigatório!" })
+        if (!password) return res.status(422).json({ message: "A senha é obrigatória!" })
+        if (password !== confirmpassword) return res.status(422).json({ message: "As senhas não conferem!" })
 
         //check if user already exist
-        const userExist = await User.findOne({ email: email})
+        const userExist = await User.findOne({ email: email })
 
-        if(userExist) return res.status(422).json({message: "Por favor, utilize outro e-mail!"})
+        if (userExist) return res.status(422).json({ message: "Por favor, utilize outro e-mail!" })
 
         //create a password
         const salt = await bcrypt.genSalt(12);
@@ -37,28 +38,28 @@ module.exports = class UserController {
 
             console.log(newUser)
         } catch (error) {
-            res.status(500).json({message: error})
+            res.status(500).json({ message: error })
         }
     }
     static async login(req, res) {
 
-        const {email, password} = req.body
+        const { email, password } = req.body
 
-        if(!email) return res.status(422).json({message: "O e-mail é obrigatório"})
-        if(!password) return res.status(422).json({message: "A senha é obrigatória"})
+        if (!email) return res.status(422).json({ message: "O e-mail é obrigatório" })
+        if (!password) return res.status(422).json({ message: "A senha é obrigatória" })
 
         //check if user exists
         const user = await User.findOne({ email: email })
 
-        if(!user) return res.status(422).json({message: "Não há usuário cadastrado com este e-mail"})
+        if (!user) return res.status(422).json({ message: "Não há usuário cadastrado com este e-mail" })
 
         //check if password match with db password
         const checkPassword = await bcrypt.compare(password, user.password)
 
-        if(!checkPassword) return res.status(422).json({message: "Senha incorreta"})
+        if (!checkPassword) return res.status(422).json({ message: "Senha incorreta" })
 
         await createUserToken(user, req, res)
-        
+
     }
     static async editUser(req, res) {
         //check if user exists
@@ -70,10 +71,10 @@ module.exports = class UserController {
         //check if email has already taken
         const userExist = await User.findOne({ email: email })
 
-        if(user.email !== email && userExist) return res.status(422).json({message: "Por favor, utilize outro e-mail!"})
+        if (user.email !== email && userExist) return res.status(422).json({ message: "Por favor, utilize outro e-mail!" })
 
         if (password !== confirmpassword) {
-            return res.status(422).json({message: "As senhas não conferem"})
+            return res.status(422).json({ message: "As senhas não conferem" })
         } else if (password === confirmpassword && password != null) {
             //creating a new password
             const salt = await bcrypt.genSalt(12)
@@ -90,31 +91,44 @@ module.exports = class UserController {
                 { new: true },
             )
 
-            res.status(200).json({message: "Usuário atualizado com sucesso!"})
+            res.status(200).json({ message: "Usuário atualizado com sucesso!" })
         } catch (error) {
-            
-            res.status(500).json({message: err})
+
+            res.status(500).json({ message: err })
             return
         }
     }
     static async showUser(req, res) {
-        //check if user exists
-        const token = getToken(req)
-        const user = await getUserByToken(token)
+        const userId = req.params.id
+        const user = await User.findById(userId)
 
-        res.status(200).json({message: `O nome do usuário é ${user.name} e o e-mail é ${user.email}`})
+        res.status(200).json({ user })
+    }
+    static async likedPosts(req, res) {
+        try {
+            const userId = req.params.id
+            const user = await User.findById(userId)
+
+            const likedPostsId = user.likedPosts
+            const likedPosts = await Posts.find({ _id: likedPostsId })
+
+            res.status(200).json(likedPosts)
+        } catch (error) {
+            res.status(500).json({ message: error })
+            return
+        }
     }
     static async passwordRecovery(req, res) {
         const { email } = req.body
 
-        if(!email) return res.status(422).json({message: "O e-mail é obrigatório"})
-        
+        if (!email) return res.status(422).json({ message: "O e-mail é obrigatório" })
+
         const transport = nodemailer.createTransport({
             host: "sandbox.smtp.mailtrap.io",
             port: 2525,
             auth: {
-              user: process.env.MAIL_USER,
-              pass: process.env.MAIL_PASS
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS
             }
         });
 
@@ -127,7 +141,7 @@ module.exports = class UserController {
         };
 
         transport.sendMail(message, function (err) {
-            if(!err) return res.status(400).json({message: "Erro ao enviar o e-mail"})
+            if (!err) return res.status(400).json({ message: "Erro ao enviar o e-mail" })
         })
 
     }
