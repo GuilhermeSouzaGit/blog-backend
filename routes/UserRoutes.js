af7a157a8b379d5e3dbd464673e4ed4c71709de4
Guilherme <guisou1902@gmail.com>
2023-07-07 17:36:03 -0300

    feat: now users can see liked posts from other users

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -7,7 +7,8 @@ const verifyToken = require("../helpers/verify-token")
 router.post("/register", UserController.register)
 router.post("/login", UserController.login)
 router.patch("/edit/:id", verifyToken, UserController.editUser)
-router.get("/profile", verifyToken, UserController.showUser)
+router.get("/profile/:id", verifyToken, UserController.showUser)
+router.get("/liked/:id", verifyToken, UserController.likedPosts)
 router.post("/password/reset", verifyToken, UserController.passwordRecovery)
 
 module.exports = router
