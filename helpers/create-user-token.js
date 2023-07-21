95167d6d7eb801011fe9824053d96430ff091e3d
Guilherme <guisou1902@gmail.com>
2023-07-21 14:24:32 -0300

    feat: now when the token is created, it returns if user is admin or not

--- helpers/create-user-token.js
+++ helpers/create-user-token.js
@@ -2,20 +2,13 @@ require('dotenv').config()
 const jwt = require("jsonwebtoken")
 
 const createUserToken = async (user, req, res) => {
-    const express = require("express")
-    const cookieParser = require("cookie-parser")
-    const app = express()
-    app.use(cookieParser())
-
     const token = jwt.sign({
         name: user.name,
         id: user._id,
         isAdmin: user.isAdmin
     }, `${process.env.SECRET}`)
 
-    //return token
-    res.cookie("auth", token)
-    res.status(200).json(({ message: "Você está autenticado", token: token, userId: user._id, }))
+    res.status(200).json(({ message: "Você está autenticado", token: token, userId: user._id, admin: user.isAdmin }))
 }
 
 module.exports = createUserToken
