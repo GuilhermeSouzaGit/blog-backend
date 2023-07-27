fbf1f5574e1c1cc5135da8c8d0dee895c2442598
Guilherme <guisou1902@gmail.com>
2023-07-27 13:29:29 -0300

    feat: added a new route and imported the image upload middleware

--- routes/PostRoutes.js
+++ routes/PostRoutes.js
@@ -4,11 +4,15 @@ const PostController = require("../controllers/PostController")
 //middlewares
 const verifyToken = require("../helpers/verify-token")
 const isAdmin = require("../helpers/isAdmin")
+const imageUpload = require("../helpers/image-upload")
+
+const upload = imageUpload()
 
 router.get("/", verifyToken, PostController.getAllPosts)
 router.get("/post/:id", verifyToken, PostController.getUniquePost)
-router.post("/create", verifyToken, isAdmin, PostController.createPost)
+router.post("/create", verifyToken, isAdmin, upload.array("images"), PostController.createPost)
 router.post("/:id/like", verifyToken, PostController.like)
 router.post("/:id/comment", verifyToken, PostController.comment)
+router.get("/get/images/:id", verifyToken, PostController.getImages)
 
 module.exports = router
