f8b03056d2baba58c7aca0f7f0d2b1ad7f419a9a
Guilherme <guisou1902@gmail.com>
2023-07-27 13:05:06 -0300

    fix: removed unnecessary console.log

--- helpers/isAdmin.js
+++ helpers/isAdmin.js
@@ -3,19 +3,18 @@ const getUserByToken = require("../helpers/get-user-by-token")
 
 async function isAdmin(req, res, next) {
     const token = await getToken(req)
-    if(!token) return res.status(401).json({message: "Token não existente ou inválido"})
+    if (!token) return res.status(401).json({ message: "Token não existente ou inválido" })
     const user = await getUserByToken(token, res)
 
-    console.log(user.isAdmin, user.email)
     try {
-        if(user.isAdmin) {
+        if (user.isAdmin) {
             return next()
         } else {
-            return res.status(403).json({message: "Você não tem as permissões necessárias"})
+            return res.status(403).json({ message: "Você não tem as permissões necessárias" })
         }
     } catch (err) {
         console.log(err)
-        res.status(500).json({error: "Erro interno do servidor"})
+        res.status(500).json({ error: "Erro interno do servidor" })
     }
 
 }
