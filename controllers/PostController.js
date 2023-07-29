12e93c601513ddf6c2ebabc59ba7281ff7cde7fc
Guilherme <guisou1902@gmail.com>
2023-07-29 15:56:59 -0300

    fix: fixing the backend image upload and saving

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -8,57 +8,62 @@ const getUserByToken = require("../helpers/get-user-by-token");
 module.exports = class PostController {
     static async createPost(req, res) {
         const files = req.files;
-        const { title, content, author } = req.body
+        const { title, content, author } = req.body;
 
-        if (!title) return res.status(422).json({ message: "Para criar um post o campo title é obrigatório" })
-        if (!content) return res.status(422).json({ message: "Para criar um post o campo content é obrigatório" })
-        if (!author) return res.status(422).json({ message: "Para criar um post o campo author é obrigatório" })
+        if (!title) return res.status(422).json({ message: "Para criar um post o campo title é obrigatório" });
+        if (!content) return res.status(422).json({ message: "Para criar um post o campo content é obrigatório" });
+        if (!author) return res.status(422).json({ message: "Para criar um post o campo author é obrigatório" });
 
         try {
             const newPost = await Posts.create({
                 title,
                 content,
                 author,
-            })
-            console.log(newPost, "NEW POST")
+            });
+            console.log(newPost, "NEW POST");
 
-            const postId = newPost._id.toString()
-            console.log(postId, "POST ID")
+            const postId = newPost._id.toString();
+            console.log(postId, "POST ID");
 
             const imageUrls = [];
 
-            console.log(files)
+            console.log(files);
             if (files) {
-                try {
-                    for (const file of Object.values(files)) {
+                const uploadPromises = Object.values(files).map((file) => {
+                    return new Promise((resolve, reject) => {
                         const buffer = file.buffer;
                         const uniqueSuffix = Math.round(Math.random() * 1E9);
-                        const filename = `${postId}-${uniqueSuffix}`
+                        const filename = `${postId}-${uniqueSuffix}`;
 
-                        await cloudinary.uploader.upload_stream(
+                        cloudinary.uploader.upload_stream(
                             { folder: `post-${postId}`, public_id: filename },
                             (error, result) => {
                                 if (error) {
                                     console.error("Erro ao fazer o upload da imagem:", error);
-                                    return res.status(500).json({ message: "Erro ao fazer o upload da imagem" });
+                                    reject(error);
+                                } else {
+                                    console.log(result.url);
+                                    imageUrls.push(result.url);
+                                    resolve(result.url);
                                 }
-                                console.log(result.url);
-                                imageUrls.push(result.url)
-                                newPost.images = imageUrls;
-                            },
+                            }
                         ).end(buffer);
-                    }
-                    newPost.save()
-                } catch (error) {
-                    console.log(error);
-                }
+                    });
+                });
+
+                await Promise.all(uploadPromises);
+
+                newPost.images = imageUrls;
+                await newPost.save();
             }
-            res.status(200).json({ message: "Post criado com sucesso!" })
+
+            res.status(200).json({ message: "Post criado com sucesso!" });
         } catch (error) {
-            console.log(error, "deu erro")
-            res.status(500).json({ message: error })
+            console.log(error, "deu erro");
+            res.status(500).json({ message: error });
         }
     }
+
     static async getAllPosts(req, res) {
         const posts = await Posts.find().sort("-createdAt")
 
