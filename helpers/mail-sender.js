9d5294b9932d65bc2d43a5f643d49031fc575f4b
Guilherme <guisou1902@gmail.com>
2023-08-01 19:27:51 -0300

    chore: added console.log

--- helpers/mail-sender.js
+++ helpers/mail-sender.js
@@ -14,6 +14,8 @@ const transporter = nodemailer.createTransport({
 });
 
 const sendEmail = async (email) => {
+    console.log(email)
+    console.log(typeof email)
     const mailSent = await transporter.sendMail({
         text: "Obrigado por se cadastrar em meu site",
         subject: "Cadastro em um novo site",
